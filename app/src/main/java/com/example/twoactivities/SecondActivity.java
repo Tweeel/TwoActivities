ac952ea203caa3b899d5722830c169863df082a5
houssem eddine <75279465+Tweeel@users.noreply.github.com>
2021-07-11 19:22:50 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private EditText mReply;
+    public static final String EXTRA_REPLY = "com.example.twoactivities.extra.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.editText_main);
+        Intent intent= getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message_reply);
+        textView.setText(message);
+
+    }
+
+    public void launchFirstActivity(View view) {
+        String reply= mReply.getText().toString();
+        Intent replyIntent= new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK,replyIntent);
+        finish();
+        //startActivity(replyIntent);
+    }
+}
